5effc069e835782b588396496b2e18d838070093
B1 <cbstar@web.de>
2018-10-01 20:54:49 +0200

    added idle

---
+++ AbstractIdleTask.h
@@ -0,0 +1,18 @@
+#ifndef ABSTRACTIDLETASK_H
+#define ABSTRACTIDLETASK_H
+
+#include "AbstractTask.h"
+
+class AbstractIdleTask : public AbstractTask {
+public:
+  virtual void init() = 0;
+
+  void update() {}
+  
+  bool doUpdate() {
+	  return false;
+  }
+};
+
+#endif /* ABSTRACTIDLETASK_H */
+
